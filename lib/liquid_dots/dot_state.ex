2a9dc4b16395430c3514ad965c734ad3fef89a36
Peter Caisse <pcaisse@gmail.com>
2017-05-21 20:49:12 -0400

    Don't reuse dot ids after dots are deleted

--- lib/liquid_dots/dot_state.ex
+++ lib/liquid_dots/dot_state.ex
@@ -4,24 +4,38 @@ defmodule LiquidDots.DotState do
   """
 
   def start_link do
-    Agent.start_link(fn -> %{} end, name: __MODULE__)
+    Agent.start_link(fn -> {%{}, 0} end, name: __MODULE__)
   end
 
+  @doc """
+  Get all dots.
+  """
   def get_dots do
-    Agent.get(__MODULE__, &(&1))
+    {dots, _} = Agent.get(__MODULE__, &(&1))
+    dots
+  end
+
+  @doc """
+  Get the count of all dots that ever existed.
+  """
+  def get_count do
+    {_, count} = Agent.get(__MODULE__, &(&1))
+    count
   end
 
   def put_new_dot(dot) do
-    dots = get_dots()
-    num_dots = map_size(dots)
-    # TODO: Fix this for rapid deletion
-    new_dot_id = String.to_atom(to_string(num_dots + 1))
-    Agent.update(__MODULE__, &Map.put_new(&1, new_dot_id, dot))
+    new_count = get_count() + 1
+    new_dot_id = Integer.to_string(new_count)
+    Agent.update(__MODULE__, fn({map, _}) ->
+      {Map.put_new(map, new_dot_id, dot), new_count}
+    end)
     %{new_dot_id => dot}
   end
 
   def delete_dot(dot_id) do
-    Agent.update(__MODULE__, &Map.delete(&1, dot_id))
+    Agent.update(__MODULE__, fn({map, count}) ->
+      {Map.delete(map, dot_id), count}
+    end)
     dot_id
   end
 end
